c7d13281bd1ca0691a6118663b2d7ebe8aa5c020
SomeRandomGuy64 <58070884+SomeRandomGuy64@users.noreply.github.com>
2024-12-05 15:06:43 +0000

    Setup the logger

--- Fazon/src/Fazon.h
+++ Fazon/src/Fazon.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Fazon/Application.h"
+#include "Fazon/Log.h"
 
 // Entry Point
 #include "Fazon/EntryPoint.h"
---
+++ Fazon/src/Fazon/Log.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <memory>
+
+#include "Core.h"
+#include "spdlog/spdlog.h"
+
+namespace Fazon {
+
+	class Log {
+	public:
+		static void init();
+
+		inline static std::shared_ptr<spdlog::logger>& getCoreLogger() { return s_coreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& getClientLogger() { return s_clientLogger; }
+
+	private:
+		static std::shared_ptr<spdlog::logger> s_coreLogger;
+		static std::shared_ptr<spdlog::logger> s_clientLogger;
+	};
+
+}
+
+// Core log macros
+#define FZ_CORE_CRITICAL(...)	::Fazon::Log::getCoreLogger()->critical(__VA_ARGS__)
+#define FZ_CORE_ERROR(...)		::Fazon::Log::getCoreLogger()->error(__VA_ARGS__)
+#define FZ_CORE_WARN(...)		::Fazon::Log::getCoreLogger()->warn(__VA_ARGS__)
+#define FZ_CORE_INFO(...)		::Fazon::Log::getCoreLogger()->info(__VA_ARGS__)
+#define FZ_CORE_TRACE(...)		::Fazon::Log::getCoreLogger()->trace(__VA_ARGS__)
+#define FZ_CORE_DEBUG(...)		::Fazon::Log::getCoreLogger()->debug(__VA_ARGS__)
+
+// Client log macros
+#define FZ_CRITICAL(...)		::Fazon::Log::getClientLogger()->critical(__VA_ARGS__)
+#define FZ_ERROR(...)			::Fazon::Log::getClientLogger()->error(__VA_ARGS__)
+#define FZ_WARN(...)			::Fazon::Log::getClientLogger()->warn(__VA_ARGS__)
+#define FZ_INFO(...)			::Fazon::Log::getClientLogger()->info(__VA_ARGS__)
+#define FZ_DEBUG(...)			::Fazon::Log::getClientLogger()->debug(__VA_ARGS__)
