1a2158df596dee2f33513caeba83ffd0b10ac988
SomeRandomGuy64 <58070884+SomeRandomGuy64@users.noreply.github.com>
2024-12-06 23:51:59 +0000

    Event.h file and Key Events done

--- Fazon/src/Fazon/Core.h
+++ Fazon/src/Fazon/Core.h
@@ -1,7 +1,13 @@
 #pragma once
 
+#include <string>
+#include <functional>
+#include <sstream>
+
 #ifdef FZ_PLATFORM_WINDOWS
 	#pragma message("FZ_PLATFORM_WINDOWS is defined")
 #else
 	#error "Fazon only supports Windows!"
 #endif
+
+#define BIT(x) (1 << x)
