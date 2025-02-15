c5f7268db9dbe9c78f5d831727551b84276c6610
SomeRandomGuy64 <58070884+SomeRandomGuy64@users.noreply.github.com>
2025-02-15 02:43:33 +0000

    update fazon.h

--- Fazon/src/Fazon.h
+++ Fazon/src/Fazon.h
@@ -4,6 +4,10 @@
 #include "Fazon/Layer.h"
 #include "Fazon/ImGui/ImGuiLayer.h"
 #include "Fazon/Log.h"
+#include "Fazon/KeyCodes.h"
+#include "Fazon/MouseCodes.h"
+#include "Fazon/Input.h"
+#include "Fazon/InputState.h"
 
 // Entry Point
 #include "Fazon/EntryPoint.h"
